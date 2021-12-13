678aedf08132146cc315bd80d576cdf8f94b047f
Patrick <registmailacc@gmail.com>
2021-12-13 19:14:12 +0100

    changed opacity of fader to 0

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-'use strict'
+'use strict';
 
 // elements
-const nav = document.querySelector('.nav')
-const fadeBox = document.querySelector('.fader__textbox')
-const headerImg = document.querySelector('header')
-const contact = document.querySelector('.impressum__contact')
+const nav = document.querySelector('.nav');
+const fadeBox = document.querySelector('.fader__textbox');
+const headerImg = document.querySelector('header');
+const contact = document.querySelector('.impressum__contact');
 
 // expand contact
 contact.addEventListener('click', function () {
   if (contact.textContent == 'Contact') {
-    contact.textContent = 'maurerpatrick@protonmail.com'
-  } else contact.textContent = 'Contact'
-})
+    contact.textContent = 'maurerpatrick@protonmail.com';
+  } else contact.textContent = 'Contact';
+});
 
 // fadeBox
-let faderIndex = 0
+let faderIndex = 0;
 let faderText = [
   'a professional scientist.',
   'a curious developer.',
   'a passionate hiker.',
-]
+];
 
 function fader() {
-  fadeBox.classList.add('hide')
+  fadeBox.classList.add('hide');
   setTimeout(function () {
-    fadeBox.textContent = faderText[faderIndex]
-    fadeBox.classList.remove('hide')
-    faderIndex++
+    fadeBox.textContent = faderText[faderIndex];
+    fadeBox.classList.remove('hide');
+    faderIndex++;
     if (faderIndex == faderText.length) {
-      faderIndex = 0
+      faderIndex = 0;
     }
-  }, 1000)
+  }, 1000);
 }
 
 // sticky navigation
 const obsCallback = function (entries) {
-  const [entry] = entries
-  if (!entry.isIntersecting) nav.classList.add('sticky')
-  else nav.classList.remove('sticky')
-}
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
 const obsOptions = {
   root: null,
   treshold: 0,
-}
-const headerObserver = new IntersectionObserver(obsCallback, obsOptions)
-headerObserver.observe(headerImg)
+};
+const headerObserver = new IntersectionObserver(obsCallback, obsOptions);
+headerObserver.observe(headerImg);
